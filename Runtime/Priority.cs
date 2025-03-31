6a1845ff231effa7b2984eeccf747cc20f69212b
Adrian Yip <adrianyip1222@gmail.com>
2025-03-31 13:38:11 +0800

    Enlarged the range of the priority.

--- Runtime/Priority.cs
+++ Runtime/Priority.cs
@@ -2,10 +2,10 @@ namespace AYip.Events
 {
     public enum Priority
     {
-        Lowest = -2000,
-        Low = -1000,
+        Lowest = int.MinValue,
+        Low = -10000,
         Unset = 0,
-        High = 1000,
-        Highest = 2000,
+        High = 10000,
+        Highest = int.MinValue,
     }
 }
