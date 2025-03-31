b0d2b39f8d9ccbdbc6c7154aa582a0337ffb430a
Adrian Yip <adrianyip1222@gmail.com>
2025-03-31 13:36:14 +0800

    Support priority on subscription.

---
+++ Runtime/Priority.cs
@@ -0,0 +1,11 @@
+namespace AYip.Events
+{
+    public enum Priority
+    {
+        Lowest = -2000,
+        Low = -1000,
+        Unset = 0,
+        High = 1000,
+        Highest = 2000,
+    }
+}
